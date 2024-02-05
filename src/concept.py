c41ff92ae14873d16221f8c9fc4a069e82b9192e
Ansel Blume <6370823+AnselBlume@users.noreply.github.com>
2024-02-04 18:27:19 -0600

    Update addition and removal mutators for Concept to handle is_necessary

--- src/concept.py
+++ src/concept.py
@@ -20,16 +20,19 @@ class Concept:
     # TODO Make these all torch.Parameters
 
     # All attributes
-    zs_attrs: list[ZeroShotAttribute] = []
-    learned_attrs: list[LearnedAttribute] = []
+    necsry_zs_attrs: list[ZeroShotAttribute] = []
+    necsry_learned_attrs: list[LearnedAttribute] = []
+
+    descr_zs_attrs: list[ZeroShotAttribute] = []
+    descr_learned_attrs: list[LearnedAttribute] = []
 
     # Individual weights for necessary attributes
-    nec_zs_attr_weights: list[float] = [] # Zero shot, necessary attribute weights
-    nec_learned_attr_weights: list[float] = [] # Learned, necessary attribute weights
+    necsry_zs_attr_weights: list[float] = [] # Zero shot, necessary attribute weights
+    necsry_learned_attr_weights: list[float] = [] # Learned, necessary attribute weights
 
     # Aggregate weights for necessary attributes
-    nec_zs_attr_score_weight: float = .5
-    nec_learned_attr_score_weight: float = .5
+    necsry_zs_attr_score_weight: float = .5
+    necsry_learned_attr_score_weight: float = .5
 
     # Individual weights for descriptive attributes
     descr_zs_attr_weights: list[float] = []
@@ -39,31 +42,59 @@ class Concept:
     descr_zs_attr_score_weight: float = .5
     descr_learned_attr_score_weight: float = .5
 
-    def add_zs_attr(self, zs_attr: ZeroShotAttribute, weight: float):
-        # Add in sorted order based on name
-        idx = bisect.bisect_left([attr.name for attr in self.zs_attrs], zs_attr.name)
+    def add_zs_attr(self, attr: ZeroShotAttribute, weight: float):
+        zs_attrs = self.necsry_zs_attrs if attr.is_necessary else self.descr_zs_attrs
+        zs_attr_weights = self.necsry_zs_attr_weights if attr.is_necessary else self.descr_zs_attr_weights
 
-        self.zs_attrs.insert(idx, zs_attr)
-        self.zs_attr_weights.insert(idx, weight)
+        # Add in sorted order based on name
+        idx = bisect.bisect_left([attr.name for attr in zs_attrs], attr.name)
+
+        zs_attrs.insert(idx, attr)
+        zs_attr_weights.insert(idx, weight)
 
     def remove_zs_attr(self, name: str):
-        idx = bisect.bisect_left([attr.name for attr in self.zs_attrs], name)
+        if name in [attr.name for attr in self.necsry_zs_attrs]: # Necessary attribute
+            zs_attrs = self.necsry_zs_attrs
+            zs_attr_weights = self.necsry_zs_attr_weights
 
-        self.zs_attrs.pop(idx)
-        self.zs_attr_weights.pop(idx)
+        else: # Descriptive attribute
+            zs_attrs = self.descr_zs_attrs
+            zs_attr_weights = self.descr_zs_attr_weights
 
-    def add_learned_attr(self, learned_attr: LearnedAttribute, weight: float):
+        idx = [attr.name for attr in zs_attrs].index(name)
+
+        zs_attrs.pop(idx)
+        zs_attr_weights.pop(idx)
+
+    def add_learned_attr(self, attr: LearnedAttribute, weight: float):
+        learned_attrs = self.necsry_learned_attrs if attr.is_necessary else self.descr_learned_attrs
+        learned_attr_weights = self.necsry_learned_attr_weights if attr.is_necessary else self.descr_learned_attr_weights
+
         # Add in sorted order based on name
-        idx = bisect.bisect_left([attr.name for attr in self.learned_attrs], learned_attr.name)
+        idx = bisect.bisect_left([attr.name for attr in learned_attrs], attr.name)
 
-        self.learned_attrs.insert(idx, learned_attr)
-        self.learned_attr_weights.insert(idx, weight)
+        learned_attrs.insert(idx, attr)
+        learned_attr_weights.insert(idx, weight)
 
     def remove_learned_attr(self, name: str):
-        idx = bisect.bisect_left([attr.name for attr in self.learned_attrs], name)
+        if name in [attr.name for attr in self.necsry_learned_attrs]:
+            learned_attrs = self.necsry_learned_attrs
+            learned_attr_weights = self.necsry_learned_attr_weights
 
-        self.learned_attrs.pop(idx)
-        self.learned_attr_weights.pop(idx)
+        else:
+            learned_attrs = self.descr_learned_attrs
+            learned_attr_weights = self.descr_learned_attr_weights
+
+        idx = [attr.name for attr in learned_attrs].index(name)
+
+        learned_attrs.pop(idx)
+        learned_attr_weights.pop(idx)
+
+    def get_zs_attrs(self) -> list[ZeroShotAttribute]:
+        return sorted(self.necsry_zs_attrs + self.descr_zs_attrs, key=lambda x: x.name)
+
+    def get_learned_attrs(self) -> list[LearnedAttribute]:
+        return sorted(self.necsry_learned_attrs + self.descr_learned_attrs, key=lambda x: x.name)
 
 @dataclass
 class ConceptSet:
