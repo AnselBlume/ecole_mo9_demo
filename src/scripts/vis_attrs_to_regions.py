ae9b5ae9e2f52a940c1cae1e85e0d2c444644199
Ansel Blume <6370823+AnselBlume@users.noreply.github.com>
2024-02-15 13:42:32 -0600

    Fully working region attribution and zs attr concept prediction scripts

--- src/scripts/vis_attrs_to_regions.py
+++ src/scripts/vis_attrs_to_regions.py
@@ -1,11 +1,11 @@
 # %%
 import os # TODO Change DesCo CUDA device here
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from controller import Controller
-from model.concept import ConceptDB
+from model.concept import ConceptKB
 from torchvision.utils import draw_segmentation_masks
 from matplotlib import colormaps
 import PIL
@@ -17,6 +17,23 @@ from tqdm import tqdm
 import logging
 
 logger = logging.getLogger(__name__)
+
+def get_class_name(path: str):
+    return os.path.splitext(os.path.basename(path))[0].split('_')[0].lower()
+
+def get_all_classes(in_dir: str):
+    paths = sorted(os.listdir(in_dir))
+    classes = set()
+
+    for basename in paths:
+        file_name, ext = os.path.splitext(basename)
+        if ext not in ['.jpg', '.png']:
+            continue
+
+        obj_name = get_class_name(basename)
+        classes.add(obj_name)
+
+    return sorted(classes)
 
 # %%
 if __name__ == '__main__':
@@ -32,17 +49,42 @@ if __name__ == '__main__':
         build_sam(),
         build_desco(),
         build_zero_shot_attr_predictor(*build_clip()),
-        ConceptDB()
+        ConceptKB()
     )
 
     colormap = colormaps['rainbow']
 
     # %%
     in_dir = '/shared/nas2/blume5/fa23/ecole/src/mo9_demo/assets/graduate_descent'
-    out_dir = '/shared/nas2/blume5/fa23/ecole/results/2_14_24-region_maps-only_required'
+    out_dir = '/shared/nas2/blume5/fa23/ecole/results/2_14_24-region_maps-only_required_with_class'
+    encode_class_in_attr = True
+    include_likely_attrs = False
 
     os.makedirs(out_dir, exist_ok=True)
 
+    # Attributes for all classes
+    all_classes = get_all_classes(in_dir)
+
+    class_to_zs_attrs = {
+        class_name : retrieve_attributes(class_name, controller.llm_client)
+        for class_name in all_classes
+    }
+
+    class_to_zs_attrs = { # Extract attributes from dictionary
+        k : v['required'] + (v['likely'] if include_likely_attrs else [])
+        for k, v in class_to_zs_attrs.items()
+    }
+
+    if encode_class_in_attr:
+        class_to_zs_attrs = { # Encode class name to disambiguate attribute/class collisions
+            class_name : [
+                f'{attr} of {controller._get_article(class_name)}{class_name}'
+                for attr in attrs
+            ]
+            for class_name, attrs in class_to_zs_attrs.items()
+        }
+
+    # Generate results
     paths = sorted(os.listdir(in_dir))
     prog_bar = tqdm(paths)
 
@@ -54,15 +96,12 @@ if __name__ == '__main__':
 
         file_path = os.path.join(in_dir, basename)
 
-
         img = PIL.Image.open(file_path).convert('RGB')
         img_t = pil_to_tensor(img)
 
         # Retrieve zero-shot attributes
-        obj_name = os.path.splitext(os.path.basename(file_path))[0].split('_')[0]
-
-        zs_attrs = retrieve_attributes(obj_name, controller.llm_client)
-        zs_attrs = zs_attrs['required'] # + zs_attrs['likely']
+        obj_name = get_class_name(basename)
+        zs_attrs = class_to_zs_attrs[obj_name]
 
         # Get predictions
         results = controller.predict(img, zs_attrs=zs_attrs)
@@ -80,13 +119,9 @@ if __name__ == '__main__':
             continue
 
         # Visualize results
-        print('Original image:')
-        # img.show()
         img.save(os.path.join(out_dir, f'{file_name}_original.png'))
 
-        print('Image regions:')
         image_regions = to_pil_image(image_from_masks(part_masks, superimpose_on_image=img_t))
-        # image_regions.show()
         image_regions.save(os.path.join(out_dir, f'{file_name}_regions.png'))
 
         heatmap_imgs = []
@@ -113,8 +148,8 @@ if __name__ == '__main__':
         # Show results
         nrows = math.ceil(len(heatmap_imgs) / 3)
 
-        # fig, ax = show(heatmap_imgs, subplot_titles=zs_attrs, nrows=nrows, title='Region Heatmaps')
-        # fig.savefig(os.path.join(out_dir, f'{file_name}_region_heatmaps.png'))
+        fig, ax = show(heatmap_imgs, subplot_titles=zs_attrs, nrows=nrows, title='Region Heatmaps')
+        fig.savefig(os.path.join(out_dir, f'{file_name}_region_heatmaps.png'))
 
         fig, ax = show(maximizing_region_imgs, subplot_titles=zs_attrs, nrows=nrows, title='Maximizing regions')
         fig.savefig(os.path.join(out_dir, f'{file_name}_maximizing_regions.png'))
--- src/scripts/zs_attr_concept_cls.py
+++ src/scripts/zs_attr_concept_cls.py
@@ -1,23 +1,23 @@
 # %%
 import os # TODO Change DesCo CUDA device here
-os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from controller import Controller
-from model.concept import ConceptDB
-from torchvision.utils import draw_segmentation_masks
-from matplotlib import colormaps
+from model.concept import ConceptKB
 import PIL
 from llm.attr_retrieval import retrieve_attributes
 from torchvision.transforms.functional import pil_to_tensor, to_pil_image
-from vis_utils import image_from_masks, show
+from vis_utils import image_from_masks
 import torch
 import math
 from tqdm import tqdm
 import logging
 from itertools import chain
+import json
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 
 logger = logging.getLogger(__name__)
 
@@ -73,14 +73,14 @@ if __name__ == '__main__':
         build_sam(),
         build_desco(),
         build_zero_shot_attr_predictor(*build_clip()),
-        ConceptDB()
+        ConceptKB()
     )
-
-    colormap = colormaps['rainbow']
 
     # %%
     in_dir = '/shared/nas2/blume5/fa23/ecole/src/mo9_demo/assets/graduate_descent'
-    out_dir = '/shared/nas2/blume5/fa23/ecole/results/2_14_24-region_maps-only_required'
+    out_dir = '/shared/nas2/blume5/fa23/ecole/results/2_14_24-predictions-class_names'
+    encode_class_in_attr = True
+    include_likely_attrs = False
 
     os.makedirs(out_dir, exist_ok=True)
 
@@ -93,17 +93,18 @@ if __name__ == '__main__':
     }
 
     class_to_zs_attrs = { # Extract attributes from dictionary
-        k : v['required'] # + v['likely']
+        k : v['required'] + (v['likely'] if include_likely_attrs else [])
         for k, v in class_to_zs_attrs.items()
     }
 
-    class_to_zs_attrs = { # Encode class name to disambiguate attribute/class collisions
-        class_name : [
-            f'{attr} of {controller._get_article(class_name)}{class_name}'
-            for attr in attrs
-        ]
-        for class_name, attrs in class_to_zs_attrs.items()
-    }
+    if encode_class_in_attr:
+        class_to_zs_attrs = { # Encode class name to disambiguate attribute/class collisions
+            class_name : [
+                f'{attr} of {controller._get_article(class_name)}{class_name}'
+                for attr in attrs
+            ]
+            for class_name, attrs in class_to_zs_attrs.items()
+        }
 
     all_zs_attrs = list(chain.from_iterable(class_to_zs_attrs.values())) # list[str]
 
@@ -111,12 +112,17 @@ if __name__ == '__main__':
     paths = sorted(os.listdir(in_dir))
     prog_bar = tqdm(paths)
 
+    accuracies = {'parts': 0, 'full': 0}
+    total_count = 0
+
     # %%
     for basename in prog_bar:
         prog_bar.set_description(basename)
         file_name, ext = os.path.splitext(basename)
         if ext not in ['.jpg', '.png']:
             continue
+
+        total_count += 1
 
         file_path = os.path.join(in_dir, basename)
 
@@ -132,8 +138,14 @@ if __name__ == '__main__':
         part_attr_scores = results['scores']['part_zs_scores']['raw_scores'] # (n_all_zs_attrs, n_regions)
         part_class_scores = get_class_scores(part_attr_scores, class_to_zs_attrs, controller) # (n_classes,)
 
+        if part_class_scores.argmax() == all_classes.index(class_name): # Update accuracy
+            accuracies['parts'] += 1
+
         full_attr_scores = results['scores']['full_zs_scores']['raw_scores'] # (n_all_zs_attrs,)
         full_class_scores = get_class_scores(full_attr_scores, class_to_zs_attrs, controller) # (n_classes,)
+
+        if full_class_scores.argmax() == all_classes.index(class_name): # Update accuracy
+            accuracies['full'] += 1
 
         # Visualize regions
         part_masks = results['segmentations']['part_masks']
@@ -144,71 +156,70 @@ if __name__ == '__main__':
         regions_img = image_from_masks(part_masks, combine_as_binary_mask=len(part_masks) == 0, superimpose_on_image=img_t)
 
         # Plot scores
-        fig, axs = plt.subplots(nrows=2, ncols=2)
+        # fig, axs = plt.subplots(nrows=2, ncols=2)
 
-        axs[0][0].imshow(to_pil_image(regions_img))
-        # Set title to bottom of plot
-        axs[0][0].set_title('Regions')
-        axs[0][0].axis('off')
+        fig = plt.figure()
+        gs = GridSpec(nrows=2, ncols=6, figure=fig)
 
-        y_max = axs[0][1].get_ylim()[1]
+        ax1 = fig.add_subplot(gs[0, 0:2])
+        ax1.imshow(img)
+        ax1.set_title('Original Image')
+        ax1.axis('off')
+
+        # Regions
+        ax2 = fig.add_subplot(gs[0, 2:4])
+        ax2.imshow(to_pil_image(regions_img))
+        ax2.set_title('Regions')
+        ax2.axis('off')
+
+        # axs[0][0].imshow(to_pil_image(regions_img))
+        # axs[0][0].set_title('Regions')
+        # axs[0][0].axis('off')
+
+        # Class attributes
+        ax3 = fig.add_subplot(gs[0, 4:6])
+
         class_attrs = class_to_zs_attrs[class_name]
-        axs[0][1].set_title('Class Attributes')
         attr_str = '\n'.join([f'- {attr}' for attr in class_attrs])
-        axs[0][1].text(0, y_max - .1*y_max, attr_str, fontsize=8, verticalalignment='top')
-        # for i, attr in enumerate(class_attrs):
-        #     axs[0][1].text(0, y_max - y_max / len(class_attrs) * i, f'- {attr}, fontsize=8, verticalalignment='top')
-        axs[0][1].axis('off')
 
-        axs[1][0].barh(all_classes, part_class_scores)
-        axs[1][0].set_title('Part scores')
+        y_max = ax3.get_ylim()[1]
+        ax3.text(0, y_max - .1*y_max, attr_str, fontsize=8, verticalalignment='top')
+        ax3.set_title('Class Attributes', loc='left')
+        ax3.axis('off')
 
-        axs[1][1].barh(all_classes, full_class_scores)
-        axs[1][1].set_title('Full scores')
+        # y_max = axs[0][1].get_ylim()[1]
+        # axs[0][1].set_title('Class Attributes')
+        # attr_str = '\n'.join([f'- {attr}' for attr in class_attrs])
+        # axs[0][1].text(0, y_max - .1*y_max, attr_str, fontsize=8, verticalalignment='top')
+        # axs[0][1].axis('off')
+
+        # Part scores
+        ax4 = fig.add_subplot(gs[1, 0:3])
+        ax4.barh(all_classes, part_class_scores)
+        ax4.set_title('Part scores')
+
+        # axs[1][0].barh(all_classes, part_class_scores)
+        # axs[1][0].set_title('Part scores')
+
+        # Full image scores
+        ax5 = fig.add_subplot(gs[1, 3:6])
+        ax5.barh(all_classes, full_class_scores)
+        ax5.set_title('Full scores')
+
+        # axs[1][1].barh(all_classes, full_class_scores)
+        # axs[1][1].set_title('Full scores')
 
         fig.suptitle(file_name)
         fig.tight_layout()
 
-        break
+        # fig.show()
+        fig.savefig(os.path.join(out_dir, f'{file_name}.png'))
 
-        # # Visualize results
-        # print('Original image:')
-        # # img.show()
-        # img.save(os.path.join(out_dir, f'{file_name}_original.png'))
+    # Dump accuracy
+    accuracies['parts'] /= total_count
+    accuracies['full'] /= total_count
 
-        # print('Image regions:')
-        # image_regions = to_pil_image(image_from_masks(part_masks, superimpose_on_image=img_t))
-        # # image_regions.show()
-        # image_regions.save(os.path.join(out_dir, f'{file_name}_regions.png'))
+    out_file = os.path.join(out_dir, 'accuracies.json')
 
-        # heatmap_imgs = []
-        # maximizing_region_imgs = []
-        # for i, attr in enumerate(zs_attrs):
-        #     part_scores = attr_scores[i] # (n_regions,)
-        #     maximizing_region = part_scores.argmax().item()
-
-        #     # Heatmap colors from probabilities
-        #     colors = colormap(part_scores)[:,:3] # (n_regions, 3)
-        #     colors = [ # To list of tuples in range [0, 255]
-        #         tuple(int(255 * c) for c in color)
-        #         for color in colors
-        #     ]
-
-        #     # Draw masks with colors on image
-        #     heatmap_img = draw_segmentation_masks(img_t, masks=part_masks, colors=colors)
-        #     heatmap_imgs.append(heatmap_img)
-
-        #     # Draw mask of maximizing region
-        #     maximizing_region_img = image_from_masks(part_masks[maximizing_region][None,...], combine_as_binary_mask=True, superimpose_on_image=img_t)
-        #     maximizing_region_imgs.append(maximizing_region_img)
-
-        # # Show results
-        # nrows = math.ceil(len(heatmap_imgs) / 3)
-
-        # # fig, ax = show(heatmap_imgs, subplot_titles=zs_attrs, nrows=nrows, title='Region Heatmaps')
-        # # fig.savefig(os.path.join(out_dir, f'{file_name}_region_heatmaps.png'))
-
-        # fig, ax = show(maximizing_region_imgs, subplot_titles=zs_attrs, nrows=nrows, title='Maximizing regions')
-        # fig.savefig(os.path.join(out_dir, f'{file_name}_maximizing_regions.png'))
-
-    # %%
+    with open(out_file, 'w') as f:
+        json.dump(accuracies, f, indent=4)
