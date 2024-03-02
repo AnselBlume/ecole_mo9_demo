4870154f7a889d13084c99a80e1955a06e420487
Ansel Blume <6370823+AnselBlume@users.noreply.github.com>
2024-03-02 17:27:35 -0600

    Export more classes in kb_ops package __init__

--- src/kb_ops/__init__.py
+++ src/kb_ops/__init__.py
@@ -1,1 +1,5 @@
 from .build import kb_from_img_dir
+from .feature_cache import ConceptKBFeatureCacher
+from .train import ConceptKBTrainer
+from .feature_pipeline import ConceptKBFeaturePipeline
+from .dataset import ImageDataset, PresegmentedDataset, FeatureDataset
