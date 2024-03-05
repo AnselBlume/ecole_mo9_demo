b697ccb51ac1e76570178fd8a2a7bedc6b694d12
Ansel Blume <6370823+AnselBlume@users.noreply.github.com>
2024-03-04 21:04:05 -0600

    Allow set_feature_paths to specify specific concepts

--- src/scripts/utils.py
+++ src/scripts/utils.py
@@ -1,15 +1,20 @@
 import os
-from model.concept import ConceptKB
+from model.concept import ConceptKB, Concept
 from datetime import datetime
+from typing import Union
 
 def get_timestr():
     return datetime.now().strftime('%Y_%m_%d-%H:%M:%S')
 
-def set_feature_paths(concept_kb: ConceptKB, features_dir: str = None, segmentations_dir: str = None):
+def set_feature_paths(
+    concepts: Union[list[Concept], ConceptKB] = None,
+    features_dir: str = None,
+    segmentations_dir: str = None
+):
     '''
         Checks provided segmentation and feature directories for existence. If they exist, sets the
-        image_features_path and image_segmentations_path attributes of each example in the concept
-        based on the image_path of each example, which is assumed to be set.
+        image_features_path and image_segmentations_path attributes of each example in the Concepts
+        (or ConceptKB) based on the image_path of each example, which is assumed to be set.
 
         Doing so helps avoid recomputation of features and segmentations in the ConceptKBFeatureCacher,
         which will otherwise recompute these and set the paths in the examples.
@@ -19,14 +24,14 @@ def set_feature_paths(concept_kb: ConceptKB, features_dir: str = None, segmentat
 
     if features_dir and os.path.exists(features_dir):
         # Store pre-computed feature paths in concept examples
-        for concept in concept_kb:
+        for concept in concepts:
             for example in concept.examples:
                 basename = os.path.basename(os.path.splitext(example.image_path)[0]) + '.pkl'
                 example.image_features_path = os.path.join(features_dir, basename)
 
     if segmentations_dir and os.path.exists(segmentations_dir):
         # Store presegmented paths in concept examples
-        for concept in concept_kb:
+        for concept in concepts:
             for example in concept.examples:
                 basename = os.path.basename(os.path.splitext(example.image_path)[0]) + '.pkl'
                 example.image_segmentations_path = os.path.join(segmentations_dir, basename)
