44a31e960527c894ef07a26b8b1678c42954b98f
Ansel Blume <6370823+AnselBlume@users.noreply.github.com>
2024-03-07 02:17:47 -0600

    Properly working concept addition and removal from Retriever

--- src/kb_ops/retrieve.py
+++ src/kb_ops/retrieve.py
@@ -31,47 +31,71 @@ class CLIPConceptRetriever:
         self.clip_processor = clip_processor
         self.cache_path = cache_path
         self._index = None
-        self._concept_idx2lbl = {idx: concept_lbl.name.lower().strip() for idx, concept_lbl in enumerate(concepts)}
-        self._concept_lbl2idx = {concept_lbl.name.lower().strip(): idx for idx, concept_lbl in enumerate(concepts)}
 
         # Compute text embeddings and cache them so they dont have to be recomputed every time
         # the clip_model will probably be on a GPU, but for now let's store the computed embeds on the CPU
 
         # Cache path provided and exists on disk, so load it
+        if cache_path:
+            raise NotImplementedError('Need to save more than just the FAISS index (e.g. concepts, mapping)')
+
         if cache_path and os.path.exists(self.cache_path):
             logger.info(f"[ Loading cached FAISS index from {self.cache_path} ]")
             self._index = torch.load(self.cache_path)
 
         # No cache path specified, or cache doesn't exist yet. Compute index and possibly persist
         else:
-            logger.info('[ Computing text embeddings for FAISS index ]')
-            concept_names = [c.name.lower().strip() for c in concepts]
-            text_embeds = self._get_text_embeds(concept_names)  # Convert to 'numpy.ndarray' to process with faiss.normalize_L2
+            self.rebuild_index()
 
-            # Construct FAISS index
-            logger.info('[ Building FAISS index ]')
-            embed_dim = text_embeds.shape[1]
-            self._index = faiss.IndexFlatL2(embed_dim)
-            faiss.normalize_L2(text_embeds)
-            self._index.add(text_embeds)
-
-            if cache_path:
-                os.makedirs(os.path.dirname(cache_path), exist_ok=True)
-                torch.save(self._index, self.cache_path)  # Cache index to 'cache_path'
+    def _normalize_name(self, name: str):
+        return name.lower().strip()
 
     def _get_text_embeds(self, queries: list[str]):
         inputs = self.clip_processor(text=queries, images=None, return_tensors='pt', padding=True)
 
         with torch.inference_mode():
-            outputs = self.clip_model.get_text_features(**to_device(inputs, self.clip_model.device))
+            embeds = self.clip_model.get_text_features(**to_device(inputs, self.clip_model.device))
 
-        return outputs.cpu().numpy()
+        embeds = embeds.cpu().numpy()
+        faiss.normalize_L2(embeds)
+
+        return embeds
+
+    def rebuild_index(self):
+        logger.info('[ Computing text embeddings for FAISS index ]')
+        concept_names = [self._normalize_name(c.name) for c in concepts]
+
+        self.id_counter = 0
+        ids = np.arange(self.id_counter, self.id_counter + len(concept_names))
+        self.id_counter += len(concept_names)
+        self._concept_lbl2id = {name : id for name, id in zip(concept_names, ids)}
+
+        # Construct FAISS index
+        # Flat indexes don't support add_with_index, but wrapping in IndexIDMap allows it to:
+        # https://github.com/facebookresearch/faiss/wiki/Faiss-indexes
+        logger.info('[ Building FAISS index ]')
+
+        text_embeds = self._get_text_embeds(concept_names)
+        embed_dim = text_embeds.shape[1]
+        self._index = faiss.IndexIDMap(faiss.IndexFlatL2(embed_dim))
+
+        self._index.add_with_ids(text_embeds, ids)
+
+        if self.cache_path:
+            os.makedirs(os.path.dirname(self.cache_path), exist_ok=True)
+            torch.save(self._index, self.cache_path)  # Cache index to 'cache_path'
 
     def retrieve(self, query: str, top_k: int) -> List[RetrievedConcept]:
         '''
             Returns the Concepts in decreasing order of match scores (increasing distance) based on
             the query and the Concepts' names.
         '''
+        # FAISS returns the requested number of elements, just with a randomly returned element if the number
+        # requested is > index size, but with distance of a giant number
+        if top_k > len(self._concepts):
+            logger.warning(f"[ Requested top_k ({top_k}) > index size ({len(self._concepts)}). Setting top_k to {len(self._concepts)} ]")
+            top_k = len(self._concepts)
+
         # Create a search vector and search for 'top_k'
         _query_embeds = self._get_text_embeds([query])
         faiss.normalize_L2(_query_embeds)
@@ -85,35 +109,63 @@ class CLIPConceptRetriever:
         return retrieved_concepts
 
     def add_concept(self, concept: Concept, update_cache=False):
+        # See https://github.com/facebookresearch/faiss/wiki/Getting-started
         # Add to faiss index (and concept list)
-        embeds = self._get_text_embeds([concept.name.lower().strip()])
-        self.concepts.append(concept)  # Update the 'self._concepts' list
-        self._index.add(embeds)  # Update the faiss index
+        name = self._normalize_name(concept.name)
+
+        id = self.id_counter
+        self.id_counter += 1
+
+        # Update concept storage and metadata
+        self._concepts.append(concept)
+        self._concept_lbl2id[name] = id
+
+        # Update FAISS index
+        embeds = self._get_text_embeds([name])
+        self._index.add_with_ids(embeds, np.array([id]))
+
+        logger.info(f"[ {concept.name} added to FAISS index ]")
 
         if update_cache:
             if not self.cache_path:
                 raise ValueError("Cache path not provided. Cannot update cache.")
 
-            self._concept_idx2lbl[len(self._concept_idx2lbl)] = concept.name.lower().strip()
-            self._concept_lbl2idx[concept.name.lower().strip()] = len(self._concept_lbl2idx)
             torch.save(self._index, self.cache_path)
             logger.info(f"[ Added new concept ({concept.name}) into {self.cache_path}]")
 
     def remove_concept(self, concept_name: str, update_cache=False):
+        # See https://github.com/facebookresearch/faiss/wiki/Special-operations-on-indexes#removing-elements-from-an-index
         # Remove from faiss index (and concept list)
-        rid = self._concept_lbl2idx[concept_name.lower().strip()]
+        name = self._normalize_name(concept_name)
+
+        rid = self._concept_lbl2id[name]
         rid = np.array([rid], dtype=np.int64)
-        self._index.remove_ids(rid)
+
+        # Remove from FAISS index
+        n_removed = self._index.remove_ids(rid)
+        if n_removed != 1:
+            raise RuntimeError(f'Expected to remove 1 element, but removed {n_removed}')
+
+        # Remove from concept storage and metadata
+        idx = None
+        for i, concept in enumerate(self._concepts):
+            if self._normalize_name(concept.name) == name:
+                idx = i
+                break
+
+        assert idx is not None, f'Concept {name} not found in concept list; something is seriously wrong'
+
+        del self._concepts[idx]
+        del self._concept_lbl2id[name]
+
         logger.info(f"[ {concept_name} removed from FAISS index ]")
 
         if update_cache:
             if not self.cache_path:
                 raise ValueError("Cache path not provided. Cannot update cache.")
 
-            self._concept_idx2lbl[len(self._concept_idx2lbl)] = concept.name.lower().strip()
-            self._concept_lbl2idx[concept.name.lower().strip()] = len(self._concept_lbl2idx)
             torch.save(self._index, self.cache_path)
-            logger.info(f"[ Removed concept ({concept.name}) from {self.cache_path}]")
+            logger.info(f"[ Removed concept ({name}) from {self.cache_path}]")
 
 # %%
 if __name__ == "__main__":
@@ -130,11 +182,7 @@ if __name__ == "__main__":
     concept_names = [
         "Spoon", "Knife", "Hammer", "Fork", "Spatula", "Tongs", "Screwdriver", "Wrench", "Ladle", "Peeler"
     ]
-    concepts = []
-    for cname in concept_names:
-        concept = Concept()
-        concept.name = cname
-        concepts.append(concept)
+    concepts = [Concept(name=name) for name in concept_names]
 
     clip_model, clip_processor = build_clip(device=device)
     retriever = CLIPConceptRetriever(concepts,
