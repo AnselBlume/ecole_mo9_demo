b22f6104eca2418d20aea28209572e63b3086801
Ansel Blume <6370823+AnselBlume@users.noreply.github.com>
2024-03-15 03:24:48 -0500

    - Revert to base dino size - Option for dino feature padding or resizing

--- src/feature_extraction/dino_features.py
+++ src/feature_extraction/dino_features.py
@@ -4,10 +4,35 @@ from torchvision import transforms
 from typing import Sequence
 from tqdm import tqdm
 import torch
+import math
+import itertools
+import torch.nn.functional as F
+from einops import rearrange
 
-# Copied from https://github.com/facebookresearch/dinov2/blob/main/dinov2/data/transforms.py
+# Transforms copied from
+# https://github.com/facebookresearch/dinov2/blob/main/dinov2/data/transforms.py
+# and
+# https://github.com/michalsr/dino_sam/blob/0742c580bcb1fb24ad2c22bb3b629f35dabd9345/extract_features.py#L96
 IMAGENET_DEFAULT_MEAN = (0.485, 0.456, 0.406)
 IMAGENET_DEFAULT_STD = (0.229, 0.224, 0.225)
+
+class CenterPadding(torch.nn.Module):
+    def __init__(self, multiple = 14):
+        super().__init__()
+        self.multiple = multiple
+
+    def _get_pad(self, size):
+        new_size = math.ceil(size / self.multiple) * self.multiple
+        pad_size = new_size - size
+        pad_size_left = pad_size // 2
+        pad_size_right = pad_size - pad_size_left
+        return pad_size_left, pad_size_right
+
+    @torch.no_grad()
+    def forward(self, x):
+        pads = list(itertools.chain.from_iterable(self._get_pad(m) for m in x.shape[:1:-1]))
+        output = F.pad(x, pads)
+        return output
 
 class _MaybeToTensor(transforms.ToTensor):
     """
@@ -32,28 +57,41 @@ def _make_normalize_transform(
     return transforms.Normalize(mean=mean, std=std)
 
 def get_dino_transform(
+    resize_img: bool,
     *,
+    padding_multiple: int = 14, # aka DINOv2 model patch size
     resize_size: int = 256,
     interpolation=transforms.InterpolationMode.BICUBIC,
     crop_size: int = 224,
     mean: Sequence[float] = IMAGENET_DEFAULT_MEAN,
     std: Sequence[float] = IMAGENET_DEFAULT_STD,
 ) -> transforms.Compose:
-    transforms_list = [
-        transforms.Resize(resize_size, interpolation=interpolation),
-        transforms.CenterCrop(crop_size),
-        _MaybeToTensor(),
-        _make_normalize_transform(mean=mean, std=std),
-    ]
+    if resize_img:
+        transforms_list = [
+            transforms.Resize(resize_size, interpolation=interpolation),
+            transforms.CenterCrop(crop_size),
+            _MaybeToTensor(),
+            _make_normalize_transform(mean=mean, std=std),
+        ]
+
+    else:
+        transforms_list = [
+            transforms.ToTensor(),
+            lambda x: x.unsqueeze(0),
+            CenterPadding(multiple=padding_multiple),
+            transforms.Normalize(mean=mean, std=std)
+        ]
+
     return transforms.Compose(transforms_list)
 
 
 class DinoFeatureExtractor(nn.Module):
-    def __init__(self, dino: nn.Module):
+    def __init__(self, dino: nn.Module, resize_images: bool = True):
         super().__init__()
 
         self.model = dino
-        self.transform = get_dino_transform()
+        self.resize_images = resize_images
+        self.transform = get_dino_transform(resize_images)
         self.device = self.model.cls_token.device
 
     def forward(self, images: list[Image]):
@@ -63,13 +101,40 @@ class DinoFeatureExtractor(nn.Module):
             for model forward details.
         '''
         # Prepare inputs
-        inputs = torch.stack([self.transform(img) for img in images]).to(self.device)
-        outputs = self.model(inputs, is_training=True) # Set is_training=True to return all outputs
+        inputs = [self.transform(img).to(self.device) for img in images]
 
-        cls_token = outputs['x_norm_clstoken']
-        patch_tokens = outputs['x_norm_patchtokens']
+        if self.resize_images:
+            inputs = torch.stack(inputs) # (n_imgs, 3, 224, 224)
+            outputs = self.model(inputs, is_training=True) # Set is_training=True to return all outputs
 
-        return cls_token, patch_tokens
+            cls_tokens = outputs['x_norm_clstoken'] # (n_imgs, n_features)
+            patch_tokens = outputs['x_norm_patchtokens'] # (n_imgs, n_patches, n_features)
+
+            # Rearrange patch tokens
+            n_patches_h, n_patches_w = torch.tensor(inputs.shape[-2:]) // self.model.patch_size
+            patch_tokens = rearrange(patch_tokens, 'n (h w) d -> n h w d', h=n_patches_h, w=n_patches_w) # (n_imgs, n_patches_h, n_patches_w, n_features)
+
+        else: # Padding to multiple of patch_size; need to run forward separately
+            cls_tokens_l = []
+            patch_tokens_l = []
+
+            for img_t in inputs:
+                outputs = self.model(img_t, is_training=True) # Set is_training=True to return all outputs
+
+                cls_tokens = outputs['x_norm_clstoken'] # (1, n_features)
+                patch_tokens = outputs['x_norm_patchtokens'] # (1, n_patches, n_features)
+
+                # Rearrange patch tokens
+                n_patches_h, n_patches_w = torch.tensor(img_t.shape[-2:]) // self.model.patch_size
+                patch_tokens = rearrange(patch_tokens, '1 (h w) d -> h w d', h=n_patches_h, w=n_patches_w)
+
+                cls_tokens_l.append(cls_tokens)
+                patch_tokens_l.append(patch_tokens)
+
+            cls_tokens = torch.cat(cls_tokens_l, dim=0) # (n_imgs, n_features)
+            patch_tokens = patch_tokens_l # list[(n_patches_h, n_patches_w, n_features)]
+
+        return cls_tokens, patch_tokens
 
     def forward_from_tensor(self, image: torch.Tensor):
         # Normalize & crop according to DINOv2 settings for ImageNet
