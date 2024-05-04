7aea015717d40095b5ce0beafdb7d8353787f563
Ansel Blume <6370823+AnselBlume@users.noreply.github.com>
2024-05-04 02:21:36 -0500

    - Add ability to train_concept from a concept's descendants - Remove unused train_until_correct mode

--- src/kb_ops/train.py
+++ src/kb_ops/train.py
@@ -35,6 +35,30 @@ class ConceptKBExampleSampler:
 
         return all_examples, all_labels
 
+    def sample_examples(
+        self,
+        concepts: list[Concept],
+        n_examples_per_concept: int
+    ):
+        '''
+            Samples n_examples_per_concept examples from each concept in concepts.
+        '''
+        sampled_examples = []
+        sampled_labels = []
+        for concept in concepts:
+            try:
+                examples = self.rng.choice(concept.examples, n_examples_per_concept, replace=False)
+            except ValueError:
+                logger.debug(f'Not enough examples to sample from for concept {concept.name}; using all examples')
+                examples = concept.examples
+
+            sampled_examples.extend(examples)
+
+            # Add labels
+            sampled_labels.extend([concept.name] * len(examples))
+
+        return sampled_examples, sampled_labels
+
     def sample_negative_examples(
         self,
         n_pos_examples: int,
@@ -66,7 +90,7 @@ class ConceptKBExampleSampler:
                 neg_examples = self.rng.choice(neg_concept.examples, n_neg_per_concept, replace=False)
 
             except ValueError: # Not enough negative examples
-                logger.warning(f'Not enough examples to sample from for concept {neg_concept.name}; using all examples')
+                logger.debug(f'Not enough examples to sample from for concept {neg_concept.name}; using all examples')
                 neg_examples = neg_concept.examples
 
             sampled_examples.extend(neg_examples)
@@ -217,17 +241,17 @@ class ConceptKBTrainer(ConceptKBForwardBase):
     def train_concept(
         self,
         concept: Concept,
-        stopping_condition: Literal['until_correct', 'n_epochs', 'validation'] = 'n_epochs',
-        until_correct_examples: list[ConceptExample] = [],
-        min_prob_margin: float = .1,
-        n_epochs_between_predictions: int = 5,
-        sample_all_negatives: bool = False,
-        sample_only_siblings: bool = False,
-        sample_only_leaf_nodes: bool = True,
-        post_sampling_hook: Callable[[list[ConceptExample]], Any] = None,
+        *, # Force the use of kwargs after this point due to API changes
+        stopping_condition: Literal['n_epochs', 'validation'] = 'n_epochs',
         n_epochs: int = 10,
-        n_global_negatives: int = 250,
+        use_descendants_as_positives: bool = False,
+        n_sampled_positives_per_descendant: int = 3,
         use_concepts_as_negatives: bool = False,
+        sample_all_negatives: bool = False,
+        sample_only_siblings_for_negatives: bool = False,
+        sample_only_leaf_nodes_for_negatives: bool = True,
+        post_sampling_hook: Callable[[list[ConceptExample]], Any] = None,
+        n_global_negatives: int = 250,
         **train_kwargs
     ) -> dict[str, Any]:
         '''
@@ -237,24 +261,18 @@ class ConceptKBTrainer(ConceptKBForwardBase):
             Arguments:
                 concept: Concept to train.
 
-                stopping_condition: One of 'until_correct', 'n_epochs', or 'validation'.
-                    If 'until_correct', training stops when the concept correctly predicts the examples in
-                    until_correct_example_paths with a probability margin of at least min_prob_margin.
+                stopping_condition: One of 'n_epochs' or 'validation'.
                     If 'n_epochs', training will stop after n_epochs.
-
-                until_correct_examples: List of examples that the concept should correctly predict if
-                    stopping_condition is 'until_correct'.
-
-                min_prob_margin: Margin of probability (computed via softmax) over other concepts that the true concept must have
-                    for each example in until_correct_example_paths if stopping_condition is 'until_correct'.
 
                 n_epochs: Number of epochs to train for if stopping_condition is 'n_epochs'.
 
-                sampling_seed: Seed for random number generator used for sampling negative examples.
+                use_descendants_as_positives: Whether to use descendants of the concept as positive examples.
+                n_sampled_positives_per_descendant: Number of positive examples to sample per descendant.
+
+                use_concepts_as_negatives: Whether to use negative examples from other concepts in the ConceptKB.
 
                 n_global_negatives: Number of global negative examples to sample for training.
 
-                use_concepts_as_negatives: Whether to use negative examples from other concepts in the ConceptKB.
         '''
         if not concept.examples:
             raise ValueError('Concept must have examples to train')
@@ -269,14 +287,14 @@ class ConceptKBTrainer(ConceptKBForwardBase):
 
             # Potentially sample negative concepts
             if use_concepts_as_negatives:
-                if sample_only_siblings:
+                if sample_only_siblings_for_negatives:
                     neg_concepts = [
                         child
                         for parent in concept.parent_concepts.values()
                         for child in parent.child_concepts.values()
                         if child != concept
                     ]
-                elif sample_only_leaf_nodes:
+                elif sample_only_leaf_nodes_for_negatives:
                     neg_concepts = [c for c in self.concept_kb.leaf_concepts if c != concept]
                 else:
                     neg_concepts = [c for c in self.concept_kb if c != concept]
@@ -291,13 +309,23 @@ class ConceptKBTrainer(ConceptKBForwardBase):
                 neg_examples = []
                 neg_concept_names = []
 
-            all_samples = concept.examples + neg_examples
+            # Construct positive examples
+            if use_descendants_as_positives:
+                descendants = self.concept_kb.rooted_subtree(concept)
+                pos_examples, concept_names = self.sampler.sample_examples(descendants, n_sampled_positives_per_descendant)
 
-            concept_labels = [ # Handle concept-specific negatives
-                concept.name if not ex.is_negative else FeatureDataset.NEGATIVE_LABEL
-                for ex in concept.examples
+            else:
+                pos_examples = concept.examples
+                concept_names = [concept.name] * len(pos_examples)
+
+            pos_labels = [ # Handle concept-specific negatives
+                concept_name if not ex.is_negative else FeatureDataset.NEGATIVE_LABEL
+                for ex, concept_name in zip(pos_examples, concept_names)
             ]
-            all_labels = concept_labels + neg_concept_names
+
+            # Merge positive and negative examples
+            all_samples = pos_examples + neg_examples
+            all_labels = pos_labels + neg_concept_names
 
             # Assume features are already cached and get paths
             all_feature_paths = [sample.image_features_path for sample in all_samples]
@@ -330,60 +358,6 @@ class ConceptKBTrainer(ConceptKBForwardBase):
 
             else: # stopping_condition == 'until_correct'
                 raise ValueError('until_correct is disabled as images may not be linearly separable, resulting in an infinite loop')
-
-                # Create dataset
-                positive_inds = [i for i, label in enumerate(all_labels) if label == concept.name]
-                until_correct_example_paths = [ex.image_features_path for ex in until_correct_examples]
-                val_ds = FeatureDataset(until_correct_example_paths, [concept.name] * len(until_correct_example_paths))
-
-                val_dl = self._get_dataloader(val_ds, is_train=False)
-
-                predictor = ConceptKBPredictor(self.concept_kb, self.feature_pipeline)
-                curr_epoch = 0
-                target_concept_index = predictor.leaf_name_to_leaf_ind[concept.name]
-
-                while True:
-                    # Train for one epoch then check the probability margins
-                    curr_epoch += 1
-                    results = self.train(train_ds, None, n_epochs=n_epochs_between_predictions, **train_kwargs)
-
-                    # Predict validation set and check probability margin of each example for stopping condition
-                    predictions = predictor.predict(val_dl)
-
-                    all_scores = torch.stack([pred['predictors_scores'] for pred in predictions])
-                    normalized_scores = F.softmax(all_scores, dim=1)
-                    logger.debug(f'Softmax scores: {normalized_scores}')
-
-                    values, indices = normalized_scores.topk(2, dim=1) # Each of values, indices are (n_examples, 2) tensors
-
-                    logger.debug(f'Top two softmax scores: {values}')
-                    logger.debug(f'Target softmax scores: {normalized_scores[:, target_concept_index]}')
-
-                    target_sigmoid_scores = all_scores[:, target_concept_index].sigmoid()
-                    logger.debug(f'Target sigmoid scores: {target_sigmoid_scores}')
-
-                    # Stop only if for all examples, the target concept is the maximal prediction among all concepts
-                    # and it is predicted as positive
-                    is_maximal_prediction = (indices[:, 0] == target_concept_index).all()
-                    is_predicted_positive = (target_sigmoid_scores > .5).all()
-
-                    if not (is_maximal_prediction and is_predicted_positive): # Not all examples are correctly predicted
-                        # Detect whether the loss is close to zero, since this would make gradient zero
-                        train_losses = [output['loss'] for output in results['train_outputs']]
-
-                        if np.isclose(np.mean(train_losses), 0.):
-                            logger.warning(f'Train loss is close to zero; setting positive scores to zero')
-                            train_kwargs['set_score_to_zero_at_indices'] = positive_inds
-
-                        continue
-
-                    prob_margins = values[:, 0] - values[:, 1] # Top prediction prob - 2nd prediction prob
-                    logger.debug(f'Concept {concept.name} predicted examples with margins {prob_margins}')
-
-                    if (prob_margins >= min_prob_margin).all():
-                        break
-
-                logger.info(f'Concept {concept.name} correctly predicted examples with margins {prob_margins} after {curr_epoch} loops')
 
             return results
 
