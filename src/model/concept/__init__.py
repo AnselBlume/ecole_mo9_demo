4b6927160f15e1636585974c39f2a35cef002df7
Ansel Blume <6370823+AnselBlume@users.noreply.github.com>
2024-05-24 03:17:32 -0500

    Update concept module

--- src/model/concept/__init__.py
+++ src/model/concept/__init__.py
@@ -1,3 +1,3 @@
 from .concept import Concept, ConceptExample
 from .concept_kb import ConceptKB, ConceptKBConfig
-from .concept_predictor import ConceptPredictor, ConceptPredictorFeatures, ConceptPredictorOutput
+from .concept_predictor import ConceptPredictor, ConceptPredictorOutput
