a5d1d3b6515e5b13df4b8ec25e3af56253427f4e
Ansel Blume <6370823+AnselBlume@users.noreply.github.com>
2024-05-30 10:25:42 -0500

    Working hierarchical training with tiny airplane dataset

---
+++ src/scripts/train/train_hierarchical.py
@@ -0,0 +1,81 @@
+# %%
+import os # Change DesCo CUDA device here
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+# Prepend to path so starts searching at src first
+import sys
+sys.path = [os.path.join(os.path.dirname(__file__), '../..')] + sys.path
+
+from kb_ops.build_kb import label_from_path, label_from_directory
+from kb_ops import kb_from_img_dir
+import logging, coloredlogs
+from scripts.train.train_and_cls import main, parse_args, get_parser as get_base_parser
+from scripts.train.parse_hierarchy import ConceptGraphParser
+
+logger = logging.getLogger(__name__)
+coloredlogs.install(level=logging.INFO)
+
+def get_parser():
+    parser = get_base_parser()
+    parser.add_argument('--hierarchy_config', type=dict, help='The hierarchy config as a yaml string')
+    parser.add_argument('--hierarchy_config_path', type=str, help='Path to hierarchy config yaml')
+
+    return parser
+
+if __name__ == '__main__':
+    parser = get_parser()
+    args = parse_args(parser, config_str='''
+        # img_dir: /shared/nas2/blume5/fa23/ecole/src/mo9_demo/data/june_demo_2024/airplanes_v1
+        img_dir: /shared/nas2/blume5/fa23/ecole/src/mo9_demo/data/june_demo_2024/airplanes_v1_tiny
+        extract_label_from: directory
+        cache.root: /shared/nas2/blume5/fa23/ecole/cache/airplanes_v1/initial_run
+        wandb_project: ecole_june_demo_2024
+
+        train:
+            limit_global_negatives: 5
+
+        hierarchy_config:
+            concepts:
+                - airplane
+                - commercial aircraft
+                - cargo jet
+                - passenger jet
+                - biplane
+                - fighter jet
+
+                # Component concepts
+                - propulsion component
+                - wings
+                - wing-mounted engine
+                - bulky fuselage
+                - row of windows
+                - double wings
+                - fixed landing gear
+                - propeller
+                - afterburner
+                - openable nose
+
+            instance_graph:
+                airplane: ['commercial aircraft', 'biplane', 'fighter jet']
+                commercial aircraft: ['cargo jet', 'passenger jet']
+                propulsion component: ['wing-mounted engine', 'afterburner', 'propeller']
+                wings: ['double wings']
+
+            component_graph:
+                airplane: ['wings', 'propulsion component']
+                commercial aircraft: ['wing-mounted engine']
+                cargo jet: ['bulky fuselage', 'openable nose']
+                passenger jet: ['row of windows']
+                biplane: ['double wings', 'fixed landing gear', 'propeller']
+                fighter jet: ['afterburner']
+        ''')
+
+    # Construct KB from images
+    label_extractor = label_from_path if args.extract_label_from == 'path' else label_from_directory
+    concept_kb = kb_from_img_dir(args.img_dir, label_from_path_fn=label_extractor)
+
+    # Apply graph connectivity and create any concepts without images
+    graph = ConceptGraphParser().parse_graph(path=args.hierarchy_config_path, config=args.hierarchy_config)
+    graph.apply(concept_kb)
+
+    main(args, parser, concept_kb=concept_kb)
