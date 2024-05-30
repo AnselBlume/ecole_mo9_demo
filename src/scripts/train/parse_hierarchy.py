68b95350277436cb3c697bd92137d6c84b4ee012
Ansel Blume <6370823+AnselBlume@users.noreply.github.com>
2024-05-30 08:20:57 -0500

    Graph parsing from config string and ConceptKB graph construction

---
+++ src/scripts/train/parse_hierarchy.py
@@ -0,0 +1,112 @@
+import yaml
+from dataclasses import dataclass
+from model.concept import ConceptKB, Concept
+import logging
+
+logger = logging.getLogger(__file__)
+
+@dataclass
+class ConceptGraph:
+    concepts: list[str]
+    instance_graph: dict[str,str]
+    component_graph: dict[str,str]
+
+    def apply(self, concept_kb: ConceptKB):
+        self.apply_concepts(concept_kb)
+        self.apply_instance_graph(concept_kb)
+        self.apply_component_graph(concept_kb)
+
+    def apply_concepts(self, concept_kb: ConceptKB):
+        graph_concepts = dict.fromkeys(self.concepts)
+
+        for concept_name in graph_concepts: # Ensure all concepts in the graph are in the KB
+            if concept_name not in concept_kb:
+                logger.info(f'Adding concept {concept_name} to KB')
+                concept_kb.add_concept(Concept(concept_name))
+
+        for concept in concept_kb: # Remove concepts not in the graph
+            if concept.name not in graph_concepts:
+                logger.info(f'Removing concept {concept.name} from KB')
+                concept_kb.remove_concept(concept)
+
+    def apply_instance_graph(self, concept_kb: ConceptKB):
+        for concept in concept_kb:
+            concept.child_concepts.clear()
+            concept.parent_concepts.clear()
+
+        for concept_name, children in self.instance_graph.items():
+            concept = concept_kb[concept_name]
+
+            for child_name in children:
+                child = concept_kb[child_name]
+                concept.add_child_concept(child)
+
+    def apply_component_graph(self, concept_kb: ConceptKB):
+        for concept in concept_kb:
+            concept.component_concepts.clear()
+            concept.containing_concepts.clear()
+
+        for concept_name, components in self.component_graph.items():
+            concept = concept_kb[concept_name]
+
+            for component_name in components:
+                component = concept_kb[component_name]
+                concept.add_component_concept(component)
+
+class ConceptGraphParser:
+    CONCEPTS_KEY = 'concepts'
+    INSTANCE_GRAPH_KEY = 'instance_graph'
+    COMPONENT_GRAPH_KEY = 'component_graph'
+
+    def parse_graph(self, path: str = None, string: str = None, config: dict = None) -> ConceptGraph:
+        assert bool(path) + bool(string) + bool(config) == 1, 'Exactly one of path or string must be provided'
+
+        if path:
+            with open(path, 'r') as f:
+                hierarchy_dict = yaml.safe_load(f)
+        elif string:
+            hierarchy_dict: dict = yaml.safe_load(string)
+
+        else:
+            hierarchy_dict = config
+
+        concepts = hierarchy_dict.get(self.CONCEPTS_KEY, [])
+        instance_graph = hierarchy_dict.get(self.INSTANCE_GRAPH_KEY, {})
+        component_graph = hierarchy_dict.get(self.COMPONENT_GRAPH_KEY, {})
+
+        return ConceptGraph(concepts=concepts, instance_graph=instance_graph, component_graph=component_graph)
+
+'''
+concepts:
+    - airplane
+    - commercial aircraft
+    - cargo jet
+    - passenger jet
+    - biplane
+    - fighter jet
+
+    # Component concepts
+    - propulsion component
+    - wings
+    - wing-mounted engine
+    - bulky fuselage
+    - row of windows
+    - double wings
+    - fixed landing gear
+    - propeller
+    - afterburner
+    - openable nose
+
+instance_graph:
+    airplane: ['commercial aircraft', 'biplane', 'fighter jet']
+    commercial aircraft: ['cargo jet', 'passenger jet']
+    propulsion component: ['wing-mounted engine', 'afterburner', 'propeller']
+
+component_graph:
+    airplane: ['wings', 'propulsion component']
+    commercial aircraft: ['wing-mounted engine']
+    cargo jet: ['bulky fuselage', 'openable nose']
+    pasenger jet: ['row of windows']
+    biplane: ['double wings', 'fixed landing gear', 'propeller']
+    fighter jet: ['afterburner']
+'''
